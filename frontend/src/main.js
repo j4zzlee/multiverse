c1ce9721c5dc745f11564f3b35d50f5981f8c996
Le Huu Hoang Gia <gia.le@shodan.io>
2017-10-11 10:29:27 +0700

    include bootstrap

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
+require('../node_modules/bootstrap-sass/assets/stylesheets/_bootstrap.scss')
 
 Vue.config.productionTip = false
 
