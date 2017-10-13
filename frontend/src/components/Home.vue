fc35fc593e63a9b949e5f59782f7bd578fd76c6a
Le Huu Hoang Gia <gia.le@shodan.io>
2017-10-14 01:50:15 +0700

    Edit home

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -8,17 +8,6 @@
     @QuickNote="quickNote"
     @TakeNote="takeNote"
   ></fab>
-  <!-- <section class="content-header">
-    <h1>
-      {{msg}}
-      <small>Optional description</small>
-    </h1>
-    <ol class="breadcrumb">
-      <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
-      <li class="active">Here</li>
-    </ol>
-  </section> -->
-
   <!-- Main content -->
   <section class="content container-fluid">
     <div class="home">
