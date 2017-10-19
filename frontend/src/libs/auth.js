1934bf8dcda12ec61bf83ee1f79e8f52cfc74843
Le Huu Hoang Gia <gia.le@shodan.io>
2017-10-19 14:08:44 +0700

    change code style of Auth.js

--- frontend/src/libs/auth.js
+++ frontend/src/libs/auth.js
@@ -1,10 +1,13 @@
 import Store from 'store'
 import TokenModel from '@/models/token'
 const Auth = {
-  loggedIn: function () {
+  loggedIn () {
     var profile = Store.get('profile') || {}
     var token = new TokenModel(profile.tokenInfo)
     return !token.isExpired()
+  },
+  clear () {
+    Store.set('profile', {annonymous: true, userInfo: {}, tokenInfo: {}, initialized: false})
   }
 }
 export default Auth
